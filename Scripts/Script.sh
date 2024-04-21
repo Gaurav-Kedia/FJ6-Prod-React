0524809c1bf82fd4d4f6b64146deb498964c0370
Gaurav Kedia <gaurav.kedia18@Gmail.com>
2024-04-21 16:21:08 +0530

    Update Script.sh with checkout scripts

--- Scripts/Script.sh
+++ Scripts/Script.sh
@@ -1,8 +1,18 @@
+#!/bin/bash
 echo "********** Running Script .sh file **********"
+# Access the argument passed to the script (GitHub ref)
+github_ref="$1"
+# Use the GitHub ref as needed in your deployment logic
+echo "Deploying with GitHub ref: $github_ref"
 cd
 cd /home/ubuntu/React-1/FJ6-Prod-React/
-npm i
+git fetch
+git checkout tags/ $github_ref
+echo "Checked out tags/ $github_ref"
+npm i 
+echo "Installed npm packages"
 npm run build
+echo "Build completed, and now moving over files to prod location"
 sudo cp -r build/* /var/www/html/
 ls -l
 pwd
