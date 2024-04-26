78797e18a767428ed4660f72dcb80bf1a495fe6f
Gaurav Kedia <gaurav.kedia18@Gmail.com>
2024-04-26 19:39:41 +0530

    Update Script.sh with fetch followed by checkout

--- Scripts/Script.sh
+++ Scripts/Script.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 echo "********** Running Script .sh file **********"
+Tag_Ref = $1
 
 # Change directory to your React project
 cd /home/ubuntu/React-1/FJ6-Prod-React/
 
 # Install dependencies
 npm install
+
+# Fetch latest changes from remote repository
+git fetch
+
+# Checkout the specific tag
+git checkout "$Tag_Ref"  # Replace <some-tag-name> with the desired tag name
 
 # Build the project
 npm run build
